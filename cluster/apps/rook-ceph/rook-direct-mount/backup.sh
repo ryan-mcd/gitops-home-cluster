5586e1b756a0564740b4d3fd3943022317850ca5
Ryan-McD <72583999+Ryan-McD@users.noreply.github.com>
2021-12-13 04:18:13 +0000

    correct ceph backups from ceph-blockpool to replicapool

--- cluster/apps/rook-ceph/rook-direct-mount/backup.sh
+++ cluster/apps/rook-ceph/rook-direct-mount/backup.sh
@@ -2,7 +2,8 @@
 
 # PVC=airsonic-config-v1 \
 # NS=media \
-# kubectl -n rook-ceph exec -it (kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/backup-rbd.sh --rbd (k get pv/(kubectl get pv | grep "$PVC" | awk -F' ' '{print $1}') -n "${NS}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$PVC"
+# kubectl -n rook-ceph exec -it (kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/backup.sh --rbd (k get pv/(kubectl get pv | grep "$PVC" | awk -F' ' '{print $1}') -n "${NS}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$PVC"
+# kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/backup.sh --rbd $(kubectl get pv/#(kubectl get pv | grep "$PVC" | awk -F' ' '{print $1}') -n "${NS}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$PVC"
 
 # Set defaults
 NFS_MOUNTPATH="/mnt/nas-data"
@@ -49,7 +50,7 @@ if [[ -f "${NFS_MOUNTPATH}/${pvc}-${CURRENT_DATE}.tar.gz" ]]; then
 fi
 
 # ceph osd pool ls
-rbd map -p ceph-blockpool "${rbd}" | xargs -I{} mount {} "${RBD_MOUNTPATH}"
+rbd map -p replicapool "${rbd}" | xargs -I{} mount {} "${RBD_MOUNTPATH}"
 tar czvf "${NFS_MOUNTPATH}/Backups/${pvc}-${CURRENT_DATE}.tar.gz" -C "${RBD_MOUNTPATH}/" .
 umount "${RBD_MOUNTPATH}"
-rbd unmap -p ceph-blockpool "${rbd}"
+rbd unmap -p replicapool "${rbd}"
